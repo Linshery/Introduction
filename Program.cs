3dd34b2ab2b14799f6b615ff7e0b210ff07342d8
Ekaterina Denisova <154327205+Linshery@users.noreply.github.com>
2023-12-19 18:10:03 +0300

    Hw_1
    1work

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+string user;
+
+Console.WriteLine("Введи, пожалуйста, свое имя:");
+user = Console.ReadLine();
+Console.WriteLine("Hello, " + user);
